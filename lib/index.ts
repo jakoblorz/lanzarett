c68b4171149ff73ffb339129879ca475a3b03af1
jakoblorz <jakob@lorz.org>
2017-08-02 15:36:14 +0200

    added more index exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,1 +1,3 @@
 export { FileSystem } from "./FileSystem";
+export { EndpointContract, IEndpointContract } from "./Contract/EndpointContract"
+export { HttpContractServer as HttpCS } from "./Server/HttpContractServer";
