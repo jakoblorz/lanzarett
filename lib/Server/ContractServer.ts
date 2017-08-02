66167985cd2ba6c60f527974b0cbe42d0ad371b3
jakoblorz <jakob@lorz.org>
2017-08-02 15:31:55 +0200

    implemented contract mapping

--- lib/Server/ContractServer.ts
+++ lib/Server/ContractServer.ts
@@ -21,7 +21,31 @@ export abstract class ContractServer implements IContractServer {
 
     public static createRequestMapperFromContractArray(contracts: IEndpointContract[]): RequestMapperFunction {
         return async function (req: csreq.IContractServerRequest) {
-            
+            const send = req.send;
+
+            if (req.rpc === undefined) {
+                return await send(csres.ContractServerResponse.FormatError());
+            }
+
+            const contract = contracts.filter((c) => c.name === req.rpc && c.role === req.role)[0];
+            if (contract === undefined) {
+                return await send(csres.ContractServerResponse.NotFoundError());
+            }
+
+            const isMissingArguments = contract.arguments
+                .filter((arg) => req.arguments
+                    .filter((a) => a.key === arg).length === 0).length > 0;
+            if (isMissingArguments) {
+                return await send(csres.ContractServerResponse.FormatError());
+            }
+
+            const args = [];
+            Array.prototype.push.apply(args, contract.arguments
+                .map((argument) => req.arguments.filter((a) => a.key === argument)[0].value));
+
+            (contract.function.apply(null, args) as Promise<any>)
+                .then((res) => send(csres.ContractServerResponse.Success(req.role, res)))
+                .catch((res) => send(csres.ContractServerResponse.ServerError()));
         }
     }
 }
