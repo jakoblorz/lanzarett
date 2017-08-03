0a1bc9ff813b066c8ae5a024d924c41032d24f30
jakoblorz <jakob@lorz.org>
2017-08-03 12:56:59 +0200

    renamed map to mapRequest

--- lib/server/ContractServer.ts
+++ lib/server/ContractServer.ts
@@ -6,15 +6,15 @@ import { IEndpointContract } from "../contract/EndpointContract";
 export type RequestMapperFunction = (req: csreq.IContractServerRequest) => Promise<void>;
 
 export interface IContractServer {
-    map: RequestMapperFunction;
+    mapRequest: RequestMapperFunction;
 }
 
 export abstract class ContractServer implements IContractServer {
 
-    map: RequestMapperFunction;
+    mapRequest: RequestMapperFunction;
 
     constructor(contracts: IEndpointContract[]) {
-        this.map = ContractServer.createRequestMapperFromContractArray(contracts);
+        this.mapRequest = ContractServer.createRequestMapperFromContractArray(contracts);
     }
 
     public abstract start(port: number): Promise<void>;
