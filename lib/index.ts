4b2ba2836fd8f32ce954c4377cfe1d53019ee875
jakoblorz <jakob@lorz.org>
2017-08-03 18:22:48 +0200

    added export all

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,7 @@
-export { FileSystem } from "./FileSystem";
-export { EndpointContract, IEndpointContract } from "./contract/EndpointContract"
-export { HttpContractServer as HttpCS } from "./server/HttpContractServer";
+export { FileSystem} from "./FileSystem";
+export { ContractServer } from "./server/ContractServer";
+export { HttpContractServer } from "./server/HttpContractServer";
+export { EndpointContract, EndpointContractFunction, EndpointContractRoleType, IEndpointContract } from "./contract/EndpointContract"
+export { ContractServerErrorResponseCode, ContractServerResponse, ContractServerResponseType, ContractServerSuccessResponseCode, IContractServerResponse} from "./server/ContractServerResponse";
+export { ContractServerRequest, ContractServerResponseFunctionType, IContractServerRequest, IContractServerRequestArgument } from "./server/ContractServerRequest";
+export { ContractMapper, IContractMapper, RequestMapperFunctionType } from "./ContractMapper";
