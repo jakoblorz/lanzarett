0b347a88c859e17a16de2d180eb9b1b2dc9c6251
jakoblorz <jakob@lorz.org>
2017-08-03 13:54:51 +0200

    replaced createResponse with normal constructor

--- lib/server/ContractServerResponse.ts
+++ lib/server/ContractServerResponse.ts
@@ -26,47 +26,48 @@ export interface IContractServerResponse {
     message: string;
 }
 
-export class ContractServerResponse {
+/**
+ * implementation of the IContractServerResponse,
+ * adding static Error/Success Constructors
+ */
+export class ContractServerResponse implements IContractServerResponse {
 
-    /**
-     * create a response that can be sent back to the client
-     * @param code error/success code to send
-     * @param type signal content type
-     * @param message payload to send
-     */
-    private static createResponse(
-        code: ContractServerErrorResponseCode | ContractServerSuccessResponseCode,
-        type: ContractServerResponseType,
-        message: string): IContractServerResponse {
-        return { code, message, type };
+    code: ContractServerErrorResponseCode | ContractServerSuccessResponseCode;
+    type: ContractServerResponseType;
+    message: string;
+
+    constructor(code: ContractServerErrorResponseCode | ContractServerSuccessResponseCode, type: ContractServerResponseType, message: string) {
+        this.code = code;
+        this.type = type;
+        this.message = message;
     }
 
     /**
      * create a 404 not found error
      */
     public static NotFoundError(): IContractServerResponse {
-        return ContractServerResponse.createResponse(404, "string", "Not Found Error");
+        return new ContractServerResponse(404, "string", "Not Found Error");
     }
 
     /**
      * create a 403 forbidden error
      */
     public static ForbiddenError(): IContractServerResponse {
-        return ContractServerResponse.createResponse(403, "string", "Forbidden Error");
+        return new ContractServerResponse(403, "string", "Forbidden Error");
     }
 
     /**
      * create a 400 format error
      */
     public static FormatError(): IContractServerResponse {
-        return ContractServerResponse.createResponse(400, "string", "Format Error");
+        return new ContractServerResponse(400, "string", "Format Error");
     }
 
     /**
      * create a 500 general/server error
      */
     public static ServerError(): IContractServerResponse {
-        return ContractServerResponse.createResponse(500, "string", "Server Error");
+        return new ContractServerResponse(500, "string", "Server Error");
     }
 
     /**
@@ -78,28 +79,28 @@ export class ContractServerResponse {
         
         if (role === "create") { // create roles will resolve with success code 201
             return typeof data === "object" ?
-                ContractServerResponse.createResponse(201, "object", JSON.stringify(data)) :
-                ContractServerResponse.createResponse(201, "string", data);
+                new ContractServerResponse(201, "object", JSON.stringify(data)) :
+                new ContractServerResponse(201, "string", data);
 
         } else if (role === "read") { // read roles will resolve with success code 200
             return typeof data === "object" ?
-                ContractServerResponse.createResponse(200, "object", JSON.stringify(data)) :
-                ContractServerResponse.createResponse(200, "string", data);
+                new ContractServerResponse(200, "object", JSON.stringify(data)) :
+                new ContractServerResponse(200, "string", data);
 
         } else if (role === "update") { // update roles will resolve with success code 202
             return typeof data === "object" ?
-                ContractServerResponse.createResponse(202, "object", JSON.stringify(data)) :
-                ContractServerResponse.createResponse(202, "string", data);
+                new ContractServerResponse(202, "object", JSON.stringify(data)) :
+                new ContractServerResponse(202, "string", data);
 
         } else if (role === "delete") { // delete roles will resolve with success code 203
             return typeof data === "object" ?
-                ContractServerResponse.createResponse(203, "object", JSON.stringify(data)) :
-                ContractServerResponse.createResponse(203, "string", data);
+                new ContractServerResponse(203, "object", JSON.stringify(data)) :
+                new ContractServerResponse(203, "string", data);
 
         } else if (role === "ping") { // ping roles will resolve with success code 204
             return typeof data === "object" ?
-                ContractServerResponse.createResponse(204, "object", JSON.stringify(data)) :
-                ContractServerResponse.createResponse(204, "string", data);
+                new ContractServerResponse(204, "object", JSON.stringify(data)) :
+                new ContractServerResponse(204, "string", data);
         }
     }
 }
