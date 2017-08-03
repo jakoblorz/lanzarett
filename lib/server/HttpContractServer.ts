ddddff21b4680caaf2fd53a821dc0d589fee6002
jakoblorz <jakob@lorz.org>
2017-08-03 13:45:55 +0200

    added comments

--- lib/server/HttpContractServer.ts
+++ lib/server/HttpContractServer.ts
@@ -6,14 +6,25 @@ import { ContractServerResponseFunctionType, ContractServerRequest, IContractSer
 import * as http from "http";
 import { parse as urlparse } from "url";
 
+/**
+ * http protocol implementation of the ContractServer
+ */
 export class HttpContractServer extends ContractServer {
 
     server: http.Server;
 
+    /**
+     * start listening for incoming requests
+     * @param port number to indicate portnumber
+     */
     public async listen(port: number): Promise<any> {
         return new Promise<void>((resolve, reject) => this.server.listen(port, resolve));
     }
 
+    /**
+     * create a function to respond to a http request with a IContractServerResponse
+     * @param response http.ServerResponse response object
+     */
     private createResponseFunction(response: http.ServerResponse): ContractServerResponseFunctionType {
         return (res: IContractServerResponse) => new Promise<void>((resolve, reject) => {
             if (res.type === "object") {
@@ -26,21 +37,34 @@ export class HttpContractServer extends ContractServer {
         });
     }
 
+    /**
+     * extract all arguments from a http request components using
+     * the following hierachy: query - header - body
+     * @param queries query arguments from the request
+     * @param headers header arguments from the request
+     * @param body optional body arguments from the request
+     */
     private extractArgumentsFromRequest(queries: any, headers: any, body: any = {}) {
+
+        // function that pushes a element into an array only if its key is not present yet
         function pushNoShadow<T extends { key: string }>(list: T[], key: string, value: T) {
             if (!(list.filter((t) => t.key == key).length > 0)) {
                 list.push(value);
             }
         };
 
+        // prepare the arguments array
         const args: IContractServerRequestArgument[] = [];
 
+        // pushNoShadow query arguments
         Object.keys(queries).map<IContractServerRequestArgument>(
             (v) => ({ key: v, value: queries[v] })).forEach((v) => pushNoShadow(args, v.key, v));
 
+        // pushNoShadow header arguments
         Object.keys(headers).map<IContractServerRequestArgument>(
             (v) => ({ key: v, value: headers[v] })).forEach((v) => pushNoShadow(args, v.key, v));
 
+        // pushNoShadow body arguments
         Object.keys(body).map<IContractServerRequestArgument>(
             (v) => ({ key: v, value: body[v] })).forEach((v) => pushNoShadow(args, v.key, v));
 
