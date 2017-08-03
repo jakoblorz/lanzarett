5cd6ac595cb5045b1dc2751ed3c078912741bd08
jakoblorz <jakob@lorz.org>
2017-08-03 13:51:24 +0200

    added more comments

--- lib/server/ContractServerRequest.ts
+++ lib/server/ContractServerRequest.ts
@@ -3,17 +3,40 @@ import { EndpointContractRoleType } from "../contract/EndpointContract";
 
 export type ContractServerResponseFunctionType = (response: IContractServerResponse) => Promise<void>;
 
+/**
+ * arguments from the request should be stored as
+ * key-value pairs
+ */
 export interface IContractServerRequestArgument {
     key: string;
     value: any;
 }
 
+/**
+ * incomming requests should be standardised using this
+ * interface
+ */
 export interface IContractServerRequest {
+
+    /**
+     * store all arguments that were sent with the request
+     */
     arguments: IContractServerRequestArgument[];
+
+    /**
+     * callback function to respond to the client
+     */
     send: ContractServerResponseFunctionType;
+
+    /**
+     * signal request type as role
+     */
     role: EndpointContractRoleType;
 }
 
+/**
+ * simple implementation of the IContractServerRequest interface
+ */
 export class ContractServerRequest implements IContractServerRequest {
 
     arguments: IContractServerRequestArgument[];
