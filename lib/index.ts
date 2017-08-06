2f435748d9a52fa9b84a33de4d210e6fe8590f7c
jakoblorz <jakob@lorz.org>
2017-08-06 14:20:33 +0200

    structured exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,16 @@
-export { FileSystem} from "./FileSystem";
+// export the filesystem functions
+export { FileSystem } from "./FileSystem";
+
+// export the server classes, interfaces and functions
 export { ContractServer, IContractMapper, RequestMapperFunctionType } from "./server/ContractServer";
-export { HttpContractServer } from "./server/HttpContractServer";
-export { EndpointContract, EndpointContractFunction, EndpointContractRoleType, IEndpointContract } from "./contract/EndpointContract"
-export { ContractServerErrorResponseCode, ContractServerResponse, ContractServerResponseType, ContractServerSuccessResponseCode, IContractServerResponse} from "./server/ContractServerResponse";
+export { ContractServerErrorResponseCode, ContractServerResponse, ContractServerResponseType, ContractServerSuccessResponseCode, IContractServerResponse } from "./server/ContractServerResponse";
 export { ContractServerRequest, ContractServerResponseFunctionType, IContractServerRequest, IContractServerRequestArgument } from "./server/ContractServerRequest";
+export { HttpContractServer } from "./server/HttpContractServer";
+
+// export the contract classes, interfaces, types and functions
+export { EndpointContract, EndpointContractFunction, EndpointContractRoleType, IEndpointContract } from "./contract/EndpointContract"
+export { IMiddlewareContract, MiddlewareContract, MiddlewareContractAfterExecFunctionType, MiddlewareContractBeforeExecFunctionType } from "./contract/MiddlewareContract";
+export { IRoutingContract, RoutingContract } from "./contract/RoutingContract";
+
+// export the key-value store functions
 export { IKeyValueStoreGet, IKeyValueStoreSet, KeyValueStore } from "./KeyValueStore"
