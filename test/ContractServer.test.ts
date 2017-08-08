92eb84f14ef2af9b6e1aa6d14aaa39c54c4c0d14
jakoblorz <jakob@lorz.org>
2017-08-08 18:29:45 +0200

    adjusted tests to static method structure

--- test/ContractServer.test.ts
+++ test/ContractServer.test.ts
@@ -30,52 +30,52 @@ describe("Unit Testing ContractServer", () => {
     });
 
     it("should resolve with Format Error when argument list is empty", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", []));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", []));
         assert.deepEqual(response, ContractServerResponse.FormatError());
     });
 
     it("should resolve with Format Error when argument rpc is missing", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "id", value: "abc" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "id", value: "abc" }]));
         assert.deepEqual(response, ContractServerResponse.FormatError());
     });
 
     it("should resolve with Not Found Error when contract cannot be found - cause 'name'", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy0" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy0" }]));
         assert.deepEqual(response, ContractServerResponse.NotFoundError());
     });
 
     it("should resolve with Not Found Error when contract cannot be found - cause 'role'", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("create", [{ key: "rpc", value: "getdummy1" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("create", [{ key: "rpc", value: "getdummy1" }]));
         assert.deepEqual(response, ContractServerResponse.NotFoundError());
     });
 
     it("should resolve with Not Found Error when contract cannot be found - combined cause", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy0" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy0" }]));
         assert.deepEqual(response, ContractServerResponse.NotFoundError());
     });
 
     it("should resolve with Format Error when arguments are missing", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }]));
         assert.deepEqual(response, ContractServerResponse.FormatError());
     });
 
     it("should resolve with success when invoking with correct arguments", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }, { key: "id", value: "abc" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }, { key: "id", value: "abc" }]));
         assert.deepEqual(response, ContractServerResponse.Success("read", "success"));
     });
 
     it("should resolve with success when invoking with correct arguments + optional arguments", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }, { key: "id", value: "abc" }, { key: "name", value: "bcd" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }, { key: "id", value: "abc" }, { key: "name", value: "bcd" }]));
         assert.deepEqual(response, ContractServerResponse.Success("read", "success"));
     });
 
     it("should resolve with success when invoking with correct but duplicate arguments", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }, { key: "id", value: "abc" }, { key: "id", value: "bcd" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy2" }, { key: "id", value: "abc" }, { key: "id", value: "bcd" }]));
         assert.deepEqual(response, ContractServerResponse.Success("read", "success"));
     });
 
     it("should resolve with success when invoking with correct muliple arguments", async () => {
-        let response = await test.invokeMatchingContractToRequest(new ContractServerRequest("read", [{ key: "rpc", value: "getdummy3" }, { key: "id", value: "abc" }, { key: "name", value: "bcd" }]));
+        let response = await ContractServer.invokeMatchingContractToRequest(test.contracts, new ContractServerRequest("read", [{ key: "rpc", value: "getdummy3" }, { key: "id", value: "abc" }, { key: "name", value: "bcd" }]));
         assert.deepEqual(response, ContractServerResponse.Success("read", "success"));
     });
 
