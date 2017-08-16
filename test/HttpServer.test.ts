5f438d14089b8ce92c0147d408ed7a4ea8e2b249
jakoblorz <jakob@lorz.org>
2017-08-16 11:22:22 +0200

    implemented first tests #6

--- test/HttpServer.test.ts
+++ test/HttpServer.test.ts
@@ -3,12 +3,13 @@ import { HttpServer } from "../src/servers/HttpServer";
 
 import * as mocha from "mocha";
 import * as assert from "assert";
+import * as http from "supertest";
 
 class TestEndpoint
-    extends ServiceEndpoint.ServiceEndpointRoleClasses.CreateServiceEndpoint<string> {
+    extends ServiceEndpoint.ServiceEndpoint<string> {
 
-    constructor() {
-        super("test-endp", ["a", "b"], "test");
+    constructor(name: string, role: ServiceEndpoint.ServiceEndpointRole) {
+        super(name, ["a", "b"], role, "test");
     }
 
     public async call(a: string, b: string): Promise<ServiceEndpointResponse.IServiceEndpointResponse> {
@@ -18,6 +19,15 @@ class TestEndpoint
 
         return this.Success("error") as ServiceEndpointResponse.IServiceEndpointResponse;
     }
+
+    public static generateDummyEndpoints() {
+        return [
+            new this("create-dummy", "create"),
+            new this("read-dummy", "read"),
+            new this("update-dummy", "update"),
+            new this("delete-dummy", "delete")
+        ];
+    }
     
 }
 
@@ -25,8 +35,51 @@ describe("HttpServer", () => {
 
     let server: HttpServer;
     before(async () => {
-        server = new HttpServer([new TestEndpoint()]);
+        server = new HttpServer(TestEndpoint.generateDummyEndpoints());
     });
 
-    
+    it("should respond with Format Error when requesting non-api url", async () => {
+        const response = await http(server.server).get("/");
+        assert.equal(response.status, new ServiceEndpointResponse.ServiceEndpointErrorResponse
+            .FormatErrorResponse().status_code);
+        assert.deepEqual(response.body, new ServiceEndpointResponse.ServiceEndpointErrorResponse
+            .FormatErrorResponse());
+    });
+
+    TestEndpoint.generateDummyEndpoints().forEach((endpoint) => {
+
+        let method = "get";
+        switch (endpoint.role) {
+            case "create":
+                method = "post";
+                break;
+            case "read":
+                method = "get";
+                break;
+            case "update":
+                method = "put";
+                break;
+            case "delete":
+                method = "delete";
+                break;
+        }
+
+
+        it("[" + endpoint.role.toUpperCase() + "] should respond with Format Error when rpc code missing", async () => {
+            const response = await (http(server.server) as any)[method]("/api/" + endpoint.role + "?a=a&b=b");
+            assert.equal(response.status, new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .FormatErrorResponse().status_code);
+            assert.deepEqual(response.body, new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .FormatErrorResponse());
+        });
+
+        it("[" + endpoint.role.toUpperCase() + "] should response with Format Error when all arguments are missing", async () => {
+            const response = await (http(server.server) as any)[method]("/api/" + endpoint.role);
+            assert.equal(response.status, new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .FormatErrorResponse().status_code);
+            assert.deepEqual(response.body, new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .FormatErrorResponse());
+        });
+    });
+
 });
