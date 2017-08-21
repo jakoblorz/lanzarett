11bd8664e2674858c7f6a3d4347f316f2deb998e
jakoblorz <jakob@lorz.org>
2017-08-21 13:31:07 +0200

    added a wrapper function to the register function

--- lib/ServiceEndpoint.ts
+++ lib/ServiceEndpoint.ts
@@ -392,15 +392,43 @@ export class ServiceEndpointNamespace {
     }
 
     /**
-     * register
+     * register a function to be accessible from http requests
+     * @param role specify the role of this endpoint function
+     * @param name name this endpoint functions - must be unique within this namespace
+     * @param fn the actual endpoint function
+     * @param sample provide as sample of a response
      */
     public register<T = ServiceEndpointResponse.ServiceEndpointResponseDataType>(
         role: ServiceEndpoint.ServiceEndpointRole,
-        name: string, fn: (...args: any[]) => Promise<ServiceEndpointResponse.IServiceEndpointResponse>,
+        name: string, fn: (...args: any[]) => Promise<ServiceEndpointResponse.ServiceEndpointResponseDataType>,
         sample: T) {
 
+        // wrapper function to respond to the client using the correct response structure
+        const callbackFn = async (...args: any[]): Promise<ServiceEndpointResponse.IServiceEndpointResponse> => {
+            const data = await fn.apply(this, args) as ServiceEndpointResponse.ServiceEndpointResponseDataType;
+
+            if (role === "create") {
+                return new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                    .CreateServiceEndpointResponse(data) as ServiceEndpointResponse.IServiceEndpointResponse;
+
+            } else if (role === "read") {
+                return new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                    .ReadServiceEndpointResponse(data) as ServiceEndpointResponse.IServiceEndpointResponse;
+
+            } else if (role === "update") {
+                return new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                    .UpdateServiceEndpointResponse(data) as ServiceEndpointResponse.IServiceEndpointResponse;
+
+            } else {
+                return new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                    .DeleteServiceEndpointResponse(data) as ServiceEndpointResponse.IServiceEndpointResponse;
+            }
+        };
+        
+        // push this endpoint function to the list of endpoint functions, 
+        // making use of the wrapper function itself
         this.endpoints.push({
-            callback: fn,
+            callback: callbackFn,
             args: this.extractFunctionArguments(fn),
             name: this.name + "." + name,
             role: role,
