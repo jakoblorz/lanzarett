a287173285dcf1bcf2620b4c1d2cbfd77b2ce38d
jakoblorz <jakob@lorz.org>
2017-08-23 12:24:34 +0200

    moved sub-response tests into main describe callback #15

--- test/ServiceEndpointResponse.test.ts
+++ test/ServiceEndpointResponse.test.ts
@@ -32,72 +32,73 @@ describe("ServiceEndpointResponse", () => {
         const oep = new ServiceEndpointResponse.ServiceEndpointResponse<{ key: string, value: string }>({ key: "", value: "" }, 200);
         assert.equal(oep.content_type, "object");
     });
-});
 
-describe("ServiceEndpointRoleResponse", () => {
-    it("CreateServiceEndpointResponse<DataType> should set status_code to 201", async () => {
-        const cser = new ServiceEndpointResponse.ServiceEndpointRoleResponse
-            .CreateServiceEndpointResponse<string>("");
-        
-        assert.equal(cser.status_code, 201);
+
+    describe("ServiceEndpointRoleResponse", () => {
+        it("CreateServiceEndpointResponse<DataType> should set status_code to 201", async () => {
+            const cser = new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                .CreateServiceEndpointResponse<string>("");
+
+            assert.equal(cser.status_code, 201);
+        });
+
+        it("ReadServiceEndpointResponse<DataType> should set status_code to 200", async () => {
+            const rser = new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                .ReadServiceEndpointResponse<string>("");
+
+            assert.equal(rser.status_code, 200);
+        });
+
+        it("UpdateServiceEndpointResponse<DataType> should set status_code to 202", async () => {
+            const user = new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                .UpdateServiceEndpointResponse<string>("");
+
+            assert.equal(user.status_code, 202);
+        });
+
+        it("DeleteServiceEndpointResponse<DataType> should set status_code to 203", async () => {
+            const dser = new ServiceEndpointResponse.ServiceEndpointRoleResponse
+                .DeleteServiceEndpointResponse<string>("");
+
+            assert.equal(dser.status_code, 203);
+        });
     });
 
-    it("ReadServiceEndpointResponse<DataType> should set status_code to 200", async () => {
-        const rser = new ServiceEndpointResponse.ServiceEndpointRoleResponse
-            .ReadServiceEndpointResponse<string>("");
-        
-        assert.equal(rser.status_code, 200);
-    });
+    describe("ServiceEndpointErrorResponse", () => {
+        it("FormatErrorResponse: 400/string/(Format Err)", async () => {
+            const fEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .FormatErrorResponse();
 
-    it("UpdateServiceEndpointResponse<DataType> should set status_code to 202", async () => {
-        const user = new ServiceEndpointResponse.ServiceEndpointRoleResponse
-            .UpdateServiceEndpointResponse<string>("");
-        
-        assert.equal(user.status_code, 202);
-    });
+            assert.equal(fEResponse.status_code, 400);
+            assert.equal(fEResponse.content_type, "string");
+            assert.equal(fEResponse.content_data, "Format Error");
+        });
 
-    it("DeleteServiceEndpointResponse<DataType> should set status_code to 203", async () => {
-        const dser = new ServiceEndpointResponse.ServiceEndpointRoleResponse
-            .DeleteServiceEndpointResponse<string>("");
-        
-        assert.equal(dser.status_code, 203);
-    });
-});
+        it("ForbiddenErrorResponse: 403/string/(Forbidden Error)", async () => {
+            const fEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .ForbiddenErrorResponse();
 
-describe("ServiceEndpointErrorResponse", () => {
-    it("FormatErrorResponse: 400/string/(Format Err)", async () => {
-        const fEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
-            .FormatErrorResponse();
-        
-        assert.equal(fEResponse.status_code, 400);
-        assert.equal(fEResponse.content_type, "string");
-        assert.equal(fEResponse.content_data, "Format Error");
-    });
+            assert.equal(fEResponse.status_code, 403);
+            assert.equal(fEResponse.content_type, "string");
+            assert.equal(fEResponse.content_data, "Forbidden Error");
+        });
 
-    it("ForbiddenErrorResponse: 403/string/(Forbidden Error)", async () => {
-        const fEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
-            .ForbiddenErrorResponse();
-        
-        assert.equal(fEResponse.status_code, 403);
-        assert.equal(fEResponse.content_type, "string");
-        assert.equal(fEResponse.content_data, "Forbidden Error");
-    });
+        it("NotFoundErrorResponse: 404/string/(Not Found Error)", async () => {
+            const nFEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .NotFoundErrorResponse();
 
-    it("NotFoundErrorResponse: 404/string/(Not Found Error)", async () => {
-        const nFEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
-            .NotFoundErrorResponse();
-        
-        assert.equal(nFEResponse.status_code, 404);
-        assert.equal(nFEResponse.content_type, "string");
-        assert.equal(nFEResponse.content_data, "Not Found Error");
-    });
+            assert.equal(nFEResponse.status_code, 404);
+            assert.equal(nFEResponse.content_type, "string");
+            assert.equal(nFEResponse.content_data, "Not Found Error");
+        });
 
-    it("ServerErrorResponse: 500/string/(Server Error)", async () => {
-        const sEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
-            .ServerErrorResponse();
-        
-        assert.equal(sEResponse.status_code, 500);
-        assert.equal(sEResponse.content_type, "string");
-        assert.equal(sEResponse.content_data, "Server Error");
+        it("ServerErrorResponse: 500/string/(Server Error)", async () => {
+            const sEResponse = new ServiceEndpointResponse.ServiceEndpointErrorResponse
+                .ServerErrorResponse();
+
+            assert.equal(sEResponse.status_code, 500);
+            assert.equal(sEResponse.content_type, "string");
+            assert.equal(sEResponse.content_data, "Server Error");
+        });
     });
 });
