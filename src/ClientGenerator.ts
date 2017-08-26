11fe46b91ac77e5b59a537b484d0800a07331b7a
jakoblorz <jakob@lorz.org>
2017-08-26 22:31:05 +0200

    added a renderable endpoint interface

--- src/ClientGenerator.ts
+++ src/ClientGenerator.ts
@@ -5,6 +5,11 @@ import { FileSystem } from "./FileSystem";
 import * as mustache from "mustache";
 import * as path from "path";
 
+interface IRenderableServiceEndoint extends IServiceEndpoint<any, any> {
+    requestArgs: Array<{ key: string, type: string }>;
+    responseArgs: Array<{ key: string, type: string }>;
+}
+
 export class ClientGenerator {
 
     public typeDictionary: ITypeDictionary;
@@ -19,12 +24,14 @@ export class ClientGenerator {
         const template = await FileSystem.readFile(
             path.join(__dirname, "../templates/" + this.templateName + ".mustache"));
         
-        for (let i = 0; i < endpoints.length; i++){
+        for (let i = 0; i < endpoints.length; i++) {
             Object.keys(endpoints[i].request)
-                .forEach((k) => endpoints[i].request[k] = (this.typeDictionary as any)[endpoints[i].request[k]]);
+                .forEach((k) =>
+                    endpoints[i].request[k] = (this.typeDictionary as any)[endpoints[i].request[k]]);
             Object.keys(endpoints[i].response)
-                .forEach((k) => endpoints[i].response[k] = (this.typeDictionary as any)[endpoints[i].response[k]]);
-        }
+                .forEach((k) =>
+                    endpoints[i].response[k] = (this.typeDictionary as any)[endpoints[i].response[k]]);
+        }    
 
         const sdk = mustache.render(template, endpoints);
         await FileSystem.writeFile(target, sdk);
