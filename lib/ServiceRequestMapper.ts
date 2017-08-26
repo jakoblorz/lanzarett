924de68c6d92e068925bc83ef3f993e5195c59cb
jakoblorz <jakob@lorz.org>
2017-08-26 16:22:43 +0200

    implemented main mapper function

--- lib/ServiceRequestMapper.ts
+++ lib/ServiceRequestMapper.ts
@@ -1,8 +1,35 @@
 import { IServiceEndpoint } from "./interfaces/IServiceEndpoint";
+import { IServiceResponse } from "./interfaces/IServiceResponse";
+
+import { ServiceResponse } from "./ServiceResponse";
 
 export class ServiceRequestMapper {
 
     constructor(private endpoints: IServiceEndpoint<{}, {}>[]) {
 
+    }
+
+    public async mapIncomingRequestObject<RequestType extends { rpc: string }>(
+        namespace: string,
+        request: RequestType): Promise<IServiceResponse> {
+        
+        const endpoint = this.endpoints
+            .filter((v) => v.namespace === namespace)
+            .filter((v) => v.name === name)[0];
+        if (endpoint === undefined) {
+            return ServiceResponse.NotFoundError();
+        }
+
+        const isMissingArguments = Object.keys(endpoint.request)
+            .filter((k) => (request as any)[k] === undefined).length > 0;
+        if (isMissingArguments) {
+            return ServiceResponse.FormatError();
+        }
+
+        try {
+            return await endpoint.callback(request);
+        } catch (err) {
+            return ServiceResponse.ServerError();
+        }
     }
 }
